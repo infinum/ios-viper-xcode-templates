382e15760e8e0de5d1115f536f8c8cc8546dfa88
Josip Cavar <josipcavar04@gmail.com>
2021-06-16 10:06:56 +0200

    Fix warnings generated by swiftlint
    When using suggested swiflint configuration at:
    https://infinum.com/handbook/resources/ios/swiftlint.yml
    swiftlint reports warnings for:
    - force cast
    - trailing whitespace
    - missing space in comment

--- Base VIPER Interfaces/UIStoryboardExtension.swift
+++ Base VIPER Interfaces/UIStoryboardExtension.swift
@@ -4,6 +4,8 @@ extension UIStoryboard {
 
     func instantiateViewController<T: UIViewController>(ofType _: T.Type, withIdentifier identifier: String? = nil) -> T {
         let identifier = identifier ?? String(describing: T.self)
+
+        // swiftlint:disable:next force_cast
         return instantiateViewController(withIdentifier: identifier) as! T
     }
 
