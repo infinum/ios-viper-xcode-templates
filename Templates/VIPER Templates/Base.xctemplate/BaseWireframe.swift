945f81a8ffcc64ea256712f68e24896b04437b8e
Dundo7 <maroje.marcelic@gmail.com>
2021-08-30 18:35:51 +0200

    Add base VIPER to xcode templates

---
+++ Templates/VIPER Templates/Base.xctemplate/BaseWireframe.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+protocol WireframeInterface: AnyObject {
+}
+
+class BaseWireframe<ViewController> where ViewController: UIViewController {
+
+    private unowned var _viewController: ViewController
+    
+    //to retain view controller reference upon first access
+    private var temporaryStoredViewController: ViewController?
+
+    init(viewController: ViewController) {
+        temporaryStoredViewController = viewController
+        _viewController = viewController
+    }
+
+}
+
+extension BaseWireframe: WireframeInterface {
+
+}
+
+extension BaseWireframe {
+    
+    var viewController: ViewController {
+        defer { temporaryStoredViewController = nil }
+        return _viewController
+    }
+
+    var navigationController: UINavigationController? {
+        return viewController.navigationController
+    }
+
+}
+
+extension UIViewController {
+    
+    func presentWireframe<ViewController>(_ wireframe: BaseWireframe<ViewController>, animated: Bool = true, completion: (() -> Void)? = nil) {
+        present(wireframe.viewController, animated: animated, completion: completion)
+    }
+
+}
+
+extension UINavigationController {
+    
+    func pushWireframe<ViewController>(_ wireframe: BaseWireframe<ViewController>, animated: Bool = true) {
+        self.pushViewController(wireframe.viewController, animated: animated)
+    }
+    
+    func setRootWireframe<ViewController>(_ wireframe: BaseWireframe<ViewController>, animated: Bool = true) {
+        self.setViewControllers([wireframe.viewController], animated: animated)
+    }
+
+}
---
+++ Templates/VIPER Templates/Base.xctemplate/FormatterInterface.swift
@@ -0,0 +1,5 @@
+protocol FormatterInterface: AnyObject {
+}
+
+extension FormatterInterface {
+}
