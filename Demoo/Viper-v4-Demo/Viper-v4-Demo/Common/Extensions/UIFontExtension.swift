cddc876e8414a7a06a9c33418f6f1427c0735005
Zvonimir Medak <zvonimir.medak@infinum.com>
2021-10-05 09:49:23 +0200

    UIFontExtension

---
+++ Demoo/Viper-v4-Demo/Viper-v4-Demo/Common/Extensions/UIFontExtension.swift
@@ -0,0 +1,22 @@
+//
+//  UIFontExtension.swift
+//  Viper-v4-Demo
+//
+//  Created by Zvonimir Medak on 05.10.2021..
+//
+
+import UIKit
+
+extension UIFont {
+
+    static func sfProFont(ofSize size: CGFloat, weight: SFProFontWeight = .regular) -> UIFont {
+        UIFont(name: weight.rawValue, size: size) ?? .systemFont(ofSize: size)
+    }
+
+    enum SFProFontWeight: String {
+        case regular = "SFProDisplay-Regular"
+        case bold = "SFProDisplay-Bold"
+        case semibold = "SFProDisplay-Semibold"
+        case textRegular = "SFProText-Regular"
+    }
+}
