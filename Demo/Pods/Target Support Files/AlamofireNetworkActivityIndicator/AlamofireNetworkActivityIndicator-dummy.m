7c3a7e25d84c8042c5f61bf78beaed5e0182d118
Zvonimir Medak <zvonimir.medak@infinum.com>
2021-10-05 10:20:00 +0200

    removed the older version of the demo project

--- Demo/Pods/Target Support Files/AlamofireNetworkActivityIndicator/AlamofireNetworkActivityIndicator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AlamofireNetworkActivityIndicator : NSObject
-@end
-@implementation PodsDummy_AlamofireNetworkActivityIndicator
-@end
--- Demo/Pods/Target Support Files/Pods-Viper-v2-DemoTests/Pods-Viper-v2-DemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Viper_v2_DemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_Viper_v2_DemoTests
-@end
