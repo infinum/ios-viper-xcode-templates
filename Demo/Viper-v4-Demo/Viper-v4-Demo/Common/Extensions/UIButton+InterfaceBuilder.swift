92f692a20b842be32bd812f5e56914690d3c7c21
Zvonimir Medak <zvonimir.medak@infinum.com>
2021-10-05 11:21:43 +0200

    login storyboard finished, appDelegate configured and added extension for UIButton

---
+++ Demo/Viper-v4-Demo/Viper-v4-Demo/Common/Extensions/UIButton+InterfaceBuilder.swift
@@ -0,0 +1,39 @@
+//
+//  UIButton+InterfaceBuilder.swift
+//  Viper-v4-Demo
+//
+//  Created by Zvonimir Medak on 05.10.2021..
+//
+
+import UIKit
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+    }
+}
