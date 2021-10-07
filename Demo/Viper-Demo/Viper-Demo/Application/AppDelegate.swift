5bb783b4d6d4f643df05735852e421989dd1282f
Zvonimir Medak <zvonimir.medak@infinum.com>
2021-10-07 10:03:50 +0200

    added home opening from appDelegate and added constants for requestInterceptor, userDefaults and shows

--- Demo/Viper-Demo/Viper-Demo/Application/AppDelegate.swift
+++ Demo/Viper-Demo/Viper-Demo/Application/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-var window: UIWindow?
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let initialViewController = UINavigationController()
@@ -20,10 +20,21 @@ var window: UIWindow?
             return false
         }
 
-        initialViewController.setRootWireframe(LoginWireframe())
+        if shouldShowHome {
+            initialViewController.setRootWireframe(HomeWireframe())
+        } else {
+            initialViewController.setRootWireframe(LoginWireframe())
+        }
+
         window.rootViewController = initialViewController
         window.makeKeyAndVisible()
         return true
+    }
+}
+
+private extension AppDelegate {
+    var shouldShowHome: Bool {
+        UserDefaults.standard.bool(forKey: Constants.UserDefaults.remember)
     }
 }
 
--- Demo/Viper-Demo/Viper-Demo/Application/Constants.swift
+++ Demo/Viper-Demo/Viper-Demo/Application/Constants.swift
@@ -13,9 +13,26 @@ enum Constants {
         static let base = URL(string: "https://tv-shows.infinum.academy")!
         static let login = base.appendingPathComponent("/users/sign_in")
         static let register = base.appendingPathComponent("/users")
+        static let shows = base.appendingPathComponent("/shows")
     }
 
     enum UserDefaults {
         static let remember = "rememberUser"
+        static let token = "token"
+        static let expiry = "expiry"
+        static let client = "client"
+        static let uid = "uid"
+    }
+
+    enum RequestInterceptor {
+        static let uid = "uid"
+        static let accessToken = "access-token"
+        static let client = "client"
+        static let accept = "accept"
+        static let expiry = "expiry"
+        static let tokenType = "token-type"
+        static let acceptFormat = "application/json"
+        static let bearer = "Bearer"
+        static let contentType = "application/json"
     }
 }
