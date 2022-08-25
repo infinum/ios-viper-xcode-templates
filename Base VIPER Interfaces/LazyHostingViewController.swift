41584e424271e825384c1762b986629cbf98bd57
goranbrl <goran.brlas@infinum.hr>
2022-08-25 10:21:32 +0200

    Add SwiftUI templates and LazyLoadingVC

---
+++ Base VIPER Interfaces/LazyHostingViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LazyHostingViewController.swift
+//  EmergencyApp
+//
+//  Created by Goran Brlas on 24.08.2022..
+//  Copyright © 2022 Infinum. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+/// Hosting controller that should be used in modules that use SwiftUI for
+/// creating their views. We are lazily adding the root view in order to
+/// conform to our VIPER module structure.
+///
+/// The issue is that we need to
+/// pass the presenter to our SwiftUI view as an ObservedObject, but to create
+/// the presenter we need to first create the wireframe (and VC), leading to a
+/// cycle that can only be broken by this lazy loading "hack".
+///
+/// Each `LazyHostingViewController` will be generic over the root view that is
+/// contained inside it.
+class LazyHostingViewController<RootView: View>: UIViewController {
+
+    var rootView: RootView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Create the hosting controller that will hold the SwiftUI view
+        // and add it as a child view controller to self in order to properly
+        // propagate all VC lifecycle events.
+        //
+        // You can see more about child VC handling here:
+        // https://developer.apple.com/documentation/uikit/view_controllers/creating_a_custom_container_view_controller
+        let hostingController = UIHostingController(rootView: rootView)
+        view.addSubview(hostingController.view)
+        addChild(hostingController)
+
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+        hostingController.didMove(toParent: self)
+    }
+
+}
